354e1319629e78e1c5d6b979984e3602466e7c2f
Steve Pike <steve.pike@carbonculture.net>
2013-03-23 15:29:18 +0000

    fixes, better regexp, titles and more matches.

--- unitify.js
+++ unitify.js
@@ -1,33 +1,50 @@
-function fahrenheitToCentigrade(match) {  
-    var val = parseFloat(match[1]);
+function makeUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+                return v.toString(16);
+    });
+}
+
+
+function fahrenheitToCentigrade(match, unit) {  
+    var val = parseFloat(match[1] + match[2]);
     var conv = (val - 32) / 1.8;
-    return "" + conv.toFixed(2) + match[2] + "centigrade";
+    return "" + conv.toFixed(2) + match[3] + unit;
 }
 
 function getConversions(localeString) {
     // Assume we want units in the locale of localeString
-    var conversions = [
-        {
-            from: 'fahrenheit',
-            to: 'centigrade',
-            pattern: /(\d+)([A-Za-z ]*)(fahrenheit)/gi,
-            modifier: fahrenheitToCentigrade
-        },
-        {
-            from: 'farenheit',
-            to: 'centigrade',
-            pattern: /(\d+)([A-Za-z ]*)(fahrenheit)/gi,
-            modifier: fahrenheitToCentigrade
-        }
-    ];
+    var conversions = {
+        "en-gb": [
+            {
+                from: 'fahrenheit',
+                to: 'celsius',
+                pattern: /(\d+)(\.*\d*)([A-Za-z ;&]*)(fahrenheit)/gi,
+                modifier: function(match) {return fahrenheitToCentigrade(match, 'celsius')}
+            },
+            {
+                from: '°F',
+                to: '°C',
+                pattern: /(\d+)(\.*\d*)([A-Za-z ;&]*)(°F)/gi,
+                modifier: function(match) {return fahrenheitToCentigrade(match, '°C')}
+            },
+            {
+                from: '℉',
+                to: '℃',
+                pattern: /(\d+)(\.*\d*)([A-Za-z ;&]*)(℉)/gi,
+                modifier: function(match) {return fahrenheitToCentigrade(match, '℃')}
+            }
+        ]
+    };
 
-    return conversions;
+    return conversions[localeString];
 }
 
 function replaceUnits(elem) {
     // replace the text in the elements
-    var conversions = getConversions("en-gb");    
+    var conversions = getConversions("en-gb");  // how do we get locale?
     var html = $(elem).html();
+    var repls = [];
     _.each(conversions, function(_c, index, list) {
         var pattern = _c.pattern;
         var match = pattern.exec(html);
@@ -35,12 +52,18 @@ function replaceUnits(elem) {
             var converted_text = _c.modifier(match);
             console.log(converted_text);
             var repl_elem = $('<span class="converted"></span>');
+            var uuid = makeUUID()
+            repl_elem.attr('id', uuid);
+            repls.push([uuid, match[0]]);
             repl_elem.html(converted_text);
-            $(elem).html(html.replace(pattern, repl_elem[0].outerHTML));
+            $(elem).html(html.replace(match[0], repl_elem[0].outerHTML));
             html = $(elem).html();
             match = _c.pattern.exec(html);
         }
         console.log("Done")
+    });
+    _.each(repls, function(repl, ix, list) {
+        $('span#' + repl[0]).attr('title', repl[1]);
     });
 }
 
